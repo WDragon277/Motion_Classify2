9d40312c3521d9cf3296120d412d1af2d68bd7ab
WDragon277 <50436254+WDragon277@users.noreply.github.com>
2021-12-20 18:01:29 +0900

    Update repository.py
    Csv 병합이 아니라 df를 이용해서 작업하기

--- api/resource/v1/image_classify/repository.py
+++ api/resource/v1/image_classify/repository.py
@@ -22,16 +22,17 @@ def csv_merge():
     input_file = r'C:\Users\0614_\Desktop\개발용\motion_classirfy_raw_data\10. refined data\test\rf_br' # csv파일들이 있는 디렉토리 위치
     output_file = r'C:\Users\0614_\Desktop\개발용\motion_classirfy_raw_data\10. refined data\test\rf_br\total_result.csv' # 병합하고 저장하려는 파일명
 
-    allFile_list = glob.glob(os.path.join(input_file,'*')) # glob함수로 sales_로 시작하는 파일들을 모은다
+    allFile_list = glob.glob(os.path.join(input_file,'*')) # glob함수로 파일들을 모은다
     print(allFile_list)
     allData = [] # 읽어 들인 csv파일 내용을 저장할 빈 리스트를 하나 만든다
     for file in allFile_list:
         df = pd.read_csv(file) # for구문으로 csv파일들을 읽어 들인다
         allData.append(df) # 빈 리스트에 읽어 들인 내용을 추가한다
 
-    dataCombined = pd.concat(allData, axis=0, ignore_index=True) # concat함수를 이용해서 리스트의 내용을 병합
-    # axis=0은 수직으로 병합함. axis=1은 수평. ignore_index=True는 인데스 값이 기존 순서를 무시하고 순서대로 정렬되도록 한다.
-
+    scaler=MinMaxScaler()
+    scaler.fit(allData)
+    
+    minmax_df = scaler.transform(allData)
     # dataCombined <- 최대-최소 정규화 하자
 
     dataCombined.to_csv(output_file, index=False) # to_csv함수로 저장한다. 인데스를 빼려면 False로 설정
