1c736fe20604a7c4a240dca4448963b8cd60eae7
WDragon277 <50436254+WDragon277@users.noreply.github.com>
2021-12-25 19:55:58 +0900

    데이터 정제 완료 적절한 시간으로 나눠서 저장하기 작업

--- api/resource/v1/image_classify/repository.py
+++ api/resource/v1/image_classify/repository.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-  
 import functools
 
+import matplotlib as plt
 import numpy as np
 import tensorflow as tf
 import pandas as pd
@@ -9,7 +10,7 @@ import os
 
 def csv_merge():
     input_file = r'C:\Users\0614_\Desktop\개발용\motion_classirfy_raw_data\10. refined data\test\rf_br' # csv파일들이 있는 디렉토리 위치
-    output_file = r'C:\Users\0614_\Desktop\개발용\motion_classirfy_raw_data\10. refined data\test\rf_br\total_result.csv' # 병합하고 저장하려는 파일명
+    output_file = r'C:\Users\0614_\Desktop\개발용\motion_classirfy_raw_data\10. refined data\test\rf_br' # 저장하려는 위치
 
     allFile_list = glob.glob(os.path.join(input_file,'*')) # glob함수로 파일들을 모은다
     print(allFile_list)
@@ -18,17 +19,54 @@ def csv_merge():
         df = pd.read_csv(file) # for구문으로 csv파일들을 읽어 들인다
         df.columns=['ax', 'ay', 'az', 'gx', 'gy', 'gz']
         allData.append(df) # 빈 리스트에 읽어 들인 내용을 추가한다
+        datacombine = pd.concat(allData, ignore_index=True)
+        MaxMin_datacombined = (datacombine - datacombine.min()) / (datacombine.max() - datacombine.min())
+        StEnd_dataremoved = MaxMin_datacombined[100:-100]
+    return StEnd_dataremoved
+
+refined_data = csv_merge()
+Max_index=refined_data['ax'].count()
+brcount = 300
+bdcount = 385
+cbcount = 200
+wscount= 140
+split_data =pd.DataFrame()
+
+def data_label(motion):
+
+    if motion == 'bd':
+        bins = list(range(0,Max_index,bdcount))
+        bins_label = [int(x / bdcount) + 1  for x in bins]
+    elif motion == 'br':
+        bins = list(range(0, Max_index, brcount))
+        bins_label = [int(x / brcount) for x in bins]
+        refined_data['try'] = pd.cut(refined_data.index, bins, right=False, labels=bins_label[:-1])
+        split_data.append([refined_data.iloc[x:x+300] for x in bins])
+        k=0
+        for i in split_data:
+
+            i.to_csv(r'C:\Users\0614_\Desktop\개발용\motion_classirfy_raw_data\10. refined data\test\rf_br'+ str(k),header=False,index=False)
+            k = k+1
+        # split_data = pd.DataFrame(split_data)
+        # split_data.to_csv(output_file,header=False,index=False)
+
+    elif motion == 'cb':
+        bins = list(range(0, Max_index, cbcount))
+        bins_label = [int(x / cbcount) for x in bins]
+    elif motion == 'ws':
+        bins = list(range(0, Max_index, wscount))
+        bins_label = [int(x / wscount) for x in bins]
+
+        return
 
 # data normalization
 remove row with char(eng), null to 0. 
 
 #df 합치기
-a=pd.concat(allData,ignore_index=  False)
+datacombine=pd.concat(allData,ignore_index=  False)
 
 #Max-Min
 MaxMin_datacombined = (datacombine-datacombine.min()) / (datacombine.max()-datacombine.min())
-
-    return MaxMin_datacombined
 
 
 # Make pipline
